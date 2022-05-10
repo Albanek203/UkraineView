c66b5db0148b0106c8f1d2d98fa668b96eb5f1d9
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-05-10 10:33:11 +0300

    Initial commit

---
+++ View/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace View.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
