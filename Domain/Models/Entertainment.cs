a869c11799c4a59aaa893dfae12fd5dd2aea9a5d
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-08 12:33:22 +0300

    Added display name

--- Domain/Models/Entertainment.cs
+++ Domain/Models/Entertainment.cs
@@ -1,11 +1,14 @@
-﻿using Domain.Models.Abstract;
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Models.Abstract;
 
 namespace Domain.Models;
 
 public class Entertainment : Build {
     public int Id { get; set; }
     public string Name { get; set; }
+    [Display(Name = "Type")]
     public string EntertainmentType { get; set; }
+    [Display(Name = "Active")]
     public bool IsChecked { get; set; }
 
     // Information
