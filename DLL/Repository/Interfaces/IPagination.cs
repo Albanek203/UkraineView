18633a687f62f3c7771def8c400c5a4a7d13f82b
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-08 11:41:53 +0300

    Added interface for pagination

---
+++ DLL/Repository/Interfaces/IPagination.cs
@@ -0,0 +1,6 @@
+﻿namespace DLL.Repository.Interfaces;
+
+public interface IPagination<TData> where TData : class {
+    Task<int> GetCountAsync();
+    Task<IReadOnlyCollection<TData>> GetPagination(int pageNumber, int pageSize);
+}
