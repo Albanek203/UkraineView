72039bf6f7b716c929ee110b4d20fb490f091864
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-09 15:24:13 +0300

    Added async function create region

--- BLL/Services/RegionService.cs
+++ BLL/Services/RegionService.cs
@@ -7,10 +7,12 @@ public class RegionService : IReceiving<Region> {
     public async Task<IReadOnlyCollection<Region>> FindByConditionAsync(Expression<Func<Region, bool>> predicate) =>
         await _regionRepository.FindByConditionAsync(predicate);
 
+    public async Task CreateRegionAsync(Region region) => await _regionRepository.CreateAsync(region);
+
     public async Task<IReadOnlyCollection<string>> GetOnlyNameListAsync() => await _regionRepository.GetOnlyNameListAsync();
     
     public async Task<int> GetRegionCountAsync() => await _regionRepository.GetCountAsync();
 
-    public async Task<IReadOnlyCollection<Region>> GetRegionPagination(int pageNumber = 1, int pageSize = 1) =>
+    public async Task<IReadOnlyCollection<Region>> GetRegionPaginationAsync(int pageNumber = 1, int pageSize = 1) =>
         await _regionRepository.GetPagination(pageNumber, pageSize);
 }
