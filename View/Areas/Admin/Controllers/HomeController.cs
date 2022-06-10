d56209e49df16e71828771839b0ac717dcce8e0c
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-10 12:50:49 +0300

    initial admin & manager panels

---
+++ View/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,7 @@
+﻿namespace View.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Administrator")]
+public class HomeController : Controller {
+    public IActionResult Index() => View();
+}
---
+++ View/Areas/Manager/Controllers/HomeController.cs
@@ -0,0 +1,7 @@
+﻿namespace View.Areas.Manager.Controllers;
+
+[Area("Manager")]
+[Authorize(Roles = "Manager")]
+public class HomeController : Controller {
+    public IActionResult Index() => View();
+}
