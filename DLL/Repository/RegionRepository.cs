493fce107955b0e5d2e2b02ac877f72db0183a47
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-10 08:54:02 +0300

    Include MapImage value

--- DLL/Repository/RegionRepository.cs
+++ DLL/Repository/RegionRepository.cs
@@ -4,26 +4,27 @@ public class RegionRepository : BaseRepository<Region>, IPagination<Region> {
     public RegionRepository(UkraineContext context) : base(context) { }
 
     public override async Task<IReadOnlyCollection<Region>> GetAllAsync() =>
-        await this.Entities.Include(x => x.MapImage).Include(x => x.Images).Include(x => x.Entertainments)
-                  .ThenInclude(x => x.WorkTime).Include(x => x.Entertainments).ThenInclude(x => x.Address)
-                  .Include(x => x.Entertainments).ThenInclude(x => x.Reviews).ThenInclude(x => x.Images)
-                  .Include(x => x.Entertainments).ThenInclude(x => x.Images).Include(x => x.Entertainments)
-                  .ThenInclude(x => x.Contact).Include(x => x.Entertainments).ThenInclude(x => x.About)
-                  .Include(x => x.Monuments).ThenInclude(x => x.WorkTime).Include(x => x.Monuments)
-                  .ThenInclude(x => x.Address).Include(x => x.Monuments).ThenInclude(x => x.Reviews)
-                  .ThenInclude(x => x.Images).Include(x => x.Monuments).ThenInclude(x => x.Images).ToListAsync()
-                  .ConfigureAwait(false);
-
-    public override async Task<IReadOnlyCollection<Region>>
-        FindByConditionAsync(Expression<Func<Region, bool>> predicate) =>
-        await this.Entities.Include(x => x.Entertainments).ThenInclude(x => x.WorkTime).Include(x => x.Entertainments)
+        await this.Entities.Include(x => x.MapImage).Include(x => x.MapImage).Include(x => x.Images)
+                  .Include(x => x.Entertainments).ThenInclude(x => x.WorkTime).Include(x => x.Entertainments)
                   .ThenInclude(x => x.Address).Include(x => x.Entertainments).ThenInclude(x => x.Reviews)
                   .ThenInclude(x => x.Images).Include(x => x.Entertainments).ThenInclude(x => x.Images)
                   .Include(x => x.Entertainments).ThenInclude(x => x.Contact).Include(x => x.Entertainments)
                   .ThenInclude(x => x.About).Include(x => x.Monuments).ThenInclude(x => x.WorkTime)
                   .Include(x => x.Monuments).ThenInclude(x => x.Address).Include(x => x.Monuments)
                   .ThenInclude(x => x.Reviews).ThenInclude(x => x.Images).Include(x => x.Monuments)
-                  .ThenInclude(x => x.Images).Where(predicate).ToListAsync().ConfigureAwait(false);
+                  .ThenInclude(x => x.Images).ToListAsync().ConfigureAwait(false);
+
+    public override async Task<IReadOnlyCollection<Region>>
+        FindByConditionAsync(Expression<Func<Region, bool>> predicate) =>
+        await this.Entities.Include(x => x.MapImage).Include(x => x.Entertainments).ThenInclude(x => x.WorkTime)
+                  .Include(x => x.Entertainments).ThenInclude(x => x.Address).Include(x => x.Entertainments)
+                  .ThenInclude(x => x.Reviews).ThenInclude(x => x.Images).Include(x => x.Entertainments)
+                  .ThenInclude(x => x.Images).Include(x => x.Entertainments).ThenInclude(x => x.Contact)
+                  .Include(x => x.Entertainments).ThenInclude(x => x.About).Include(x => x.Monuments)
+                  .ThenInclude(x => x.WorkTime).Include(x => x.Monuments).ThenInclude(x => x.Address)
+                  .Include(x => x.Monuments).ThenInclude(x => x.Reviews).ThenInclude(x => x.Images)
+                  .Include(x => x.Monuments).ThenInclude(x => x.Images).Where(predicate).ToListAsync()
+                  .ConfigureAwait(false);
 
     public async Task<IReadOnlyCollection<string>> GetOnlyNameListAsync() =>
         await this.Entities.Select(x => x.Name).ToListAsync();
